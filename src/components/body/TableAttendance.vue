c65af5482aea684901db5ac6d1b11b102d832636
naomiaputri <naomi.aryatiputri@kompas.com>
2020-03-25 19:01:10 +0700

    attendance

---
+++ src/components/body/TableAttendance.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="row col-12 justify-content-center">
+    <div class="col-8">
+			<div class="card">
+  		  <div class="card-body p-0">
+  		    <table class="table table-striped">
+  		      <thead>
+  		        <tr>
+  		          <th>Employee</th>
+  		          <th>2020-3-01</th>
+  		          <th>2020-3-02</th>
+  		          <th>2020-3-03</th>
+  		          <th>2020-3-04</th>
+  		          <th>2020-3-05</th>
+  		        </tr>
+  		      </thead>
+  		      <tbody>
+  		        <tr>
+  		          <td>Agasta</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		        </tr>
+  		        <tr>
+  		          <td>Agasta</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		        </tr>
+  		        <tr>
+  		          <td>Agasta</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		        </tr>
+  		        <tr>
+  		          <td>Agasta</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		        </tr>
+  		        <tr>
+  		          <td>Agasta</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		          <td>12:52:30</td>
+  		        </tr>
+  		      </tbody>
+  		    </table>
+				</div>
+  		  <!-- /.card-body -->
+			</div>
+		  <div class="row justify-content-between">
+				<div class="row">	
+					<button type="button" class="btn  btn-info btn-sm">Check In</button>
+				</div>
+				<div class="row justify-content-end">	
+					<button type="button" class="btn  btn-info btn-sm">Check Out</button>
+				</div>
+			</div>
+    </div>
+  </div>
+</template>
