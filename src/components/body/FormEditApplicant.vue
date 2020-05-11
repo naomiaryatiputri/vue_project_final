86e2999906d6747df7f13343e3ca22d641bb0954
Rifky Agasta Ibrahim <rifky.agasta@kompas.com>
2020-05-11 13:12:47 +0700

    push terbaru

--- src/components/body/FormEditApplicant.vue
+++ src/components/body/FormEditApplicant.vue
@@ -160,7 +160,7 @@ export default {
 	          birth: '',
 	          apply: '',
 	          status: '',
-	          pdf:
+	          pdf: ''
 	        }
 	    }
 	},
--- src/components/body/TableApplicant.vue
+++ src/components/body/TableApplicant.vue
@@ -40,7 +40,7 @@
               <td> {{item.address}} </td>
               <td> {{item.gender}} </td>
               <td> {{item.birth}} </td>
-              <td> {{item.cv}} </td>
+              <td> {{item.apply}} </td>
               <td> {{item.status}} </td>
               <td>
                 <router-link :to="{name: 'applicantedit', params: { id: item.id }}" tag="a">
